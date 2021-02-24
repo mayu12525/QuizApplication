70d7fbbdb6eaba8178ad57457b11b5c907ea792d
Mayu <mayurichaitanya2901@gmail.com>
2021-02-24 10:41:22 +0530

    This is Quiz application in android

---
+++ Quizzer/app/src/main/java/com/example/quizzer/BookmarksAdapter.java
@@ -0,0 +1,81 @@
+package com.example.quizzer;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public  class BookmarksAdapter extends RecyclerView.Adapter <BookmarksAdapter.ViewHolder> {
+
+
+
+    private List<QuestionModel> list;
+
+    public BookmarksAdapter(List <QuestionModel> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.bookmark_item,parent,false);
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.setData(list.get(position).getQuestion(),list.get(position).getAnswer(),position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder
+    {
+
+
+        private TextView question,answer;
+        private ImageButton deleteBtn;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+            question=itemView.findViewById(R.id.question);
+            answer=itemView.findViewById(R.id.answer);
+            deleteBtn=itemView.findViewById(R.id.delete_btn);
+
+
+        }
+
+
+        private void setData(String question, String answer, final int position)
+        {
+            this.question.setText(question);
+            this.answer.setText(answer);
+
+
+            deleteBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    list.remove(position);
+                    notifyItemRemoved(position);
+                }
+            });
+        }
+    }
+
+
+}
